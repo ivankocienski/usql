7b3a0f69b4ba6188a82e074aed2bf540859f9085
Ivan Kocienski <ivan.kocienski@gmail.com>
2017-08-01 17:16:33 +0100

    Run all the tests

--- test_integrated.py
+++ test_integrated.py
@@ -58,6 +58,10 @@ class StatementResult:
     def row_count(self):
         return self._cursor.rowcount
 
+    def integer(self):
+        return self.first_record()[0]
+
+
 
 #
 # test classes
@@ -68,7 +72,27 @@ class _TestCommon(unittest.TestCase):
     def tearDown(self):
         clear_db()
 
-class TestInsert(_TestCommon):
+class _InsertCommon():
+
+    def _insert_user(self, name, sport, age):
+
+        op = Insert().\
+                q_table("users").\
+                q_columns("name", "sport", "age").\
+                q_values(name, sport, age)
+
+        res = StatementResult(op)
+        return res.last_id()
+    
+    def _insert_common_users(self):
+        r = []
+        r.append(self._insert_user("user-1", "alpha", 100))
+        r.append(self._insert_user("user-2", "beta",  101))
+        r.append(self._insert_user("user-3", "cappa", 102))
+        r.append(self._insert_user("user-4", "delta", 103))
+        return r
+
+class TestInsert(_TestCommon, _InsertCommon):
 
     def test_insert(self):
 
@@ -80,8 +104,44 @@ class TestInsert(_TestCommon):
         res = StatementResult(op)
         self.assertIsInstance(res.last_id(), int)
 
+    def test_insert_multiple(self):
 
-class TestUpdate(_TestCommon):
+        self._insert_common_users()
+
+        op = Count().q_table("users")
+        res = StatementResult(op)
+
+        self.assertEqual(res.integer(), 4)
+
+
+class TestCount(_TestCommon, _InsertCommon):
+
+    def test_count(self):
+
+        op = Insert().\
+                q_table("users").\
+                q_columns("name", "sport", "age").\
+                q_values("jerry", "alpha", 123)
+
+        res = StatementResult(op)
+
+        op = Count().q_table("users")
+        res = StatementResult(op)
+
+        self.assertEqual(res.integer(), 1)
+
+    def test_count_where(self):
+
+        self._insert_common_users()
+
+        op = Count().\
+                q_table("users").\
+                q_where("age>?", 101)
+
+        res = StatementResult(op) 
+        self.assertEqual(res.integer(), 2)
+
+class TestUpdate(_TestCommon, _InsertCommon):
 
     def test_update(self):
         op = Insert().\
@@ -100,7 +160,33 @@ class TestUpdate(_TestCommon):
         
         self.assertEqual(res.row_count(), 1)
 
-class TestDelete(_TestCommon):
+    def test_update_single(self):
+
+        r = self._insert_common_users()
+
+        op = Update().\
+                q_table("users").\
+                q_set("name", "max").\
+                q_set("age", 20).\
+                q_where("id=?", r[0])
+
+        res = StatementResult(op) 
+        self.assertEqual(res.row_count(), 1)
+
+    def test_update_multiple(self):
+
+        r = self._insert_common_users()
+
+        op = Update().\
+                q_table("users").\
+                q_set("sport", "zappa").\
+                q_where("age>?", 101)
+
+        res = StatementResult(op) 
+        self.assertEqual(res.row_count(), 2)
+
+
+class TestDelete(_TestCommon, _InsertCommon):
 
     def test_delete(self):
 
@@ -115,8 +201,44 @@ class TestDelete(_TestCommon):
                 q_from("users")
 
         res = StatementResult(op)
-        
         self.assertEqual(res.row_count(), 1)
+
+    def test_delete_multiple(self):
+
+        r = self._insert_common_users()
+
+        op = Delete().q_from("users") 
+        res = StatementResult(op)
+
+        self.assertEqual(res.row_count(), 4)
+
+    def test_delete_single(self):
+
+        r = self._insert_common_users()
+
+        op = Delete().q_from("users").q_where("id=?", r[0])
+        res = StatementResult(op)
+
+        self.assertEqual(res.row_count(), 1)
+
+        op = Count().q_table("users")
+        res = StatementResult(op)
+
+        self.assertEqual(res.integer(), 3)
+
+    def test_delete_multiple(self):
+
+        self._insert_common_users()
+
+        op = Delete().q_from("users").q_where("age>?", 101)
+        res = StatementResult(op)
+
+        self.assertEqual(res.row_count(), 2)
+
+        op = Count().q_table("users")
+        res = StatementResult(op)
+
+        self.assertEqual(res.integer(), 2)
 
 
 class TestSelect(_TestCommon):
@@ -142,10 +264,13 @@ class TestSelect(_TestCommon):
 # main
 #
 
-if __name__ == '__main__':
+def integrated_setup():
     init_db()
     make_schema()
     clear_db()
+
+if __name__ == '__main__':
+    integrated_setup()
 
     unittest.main()
 
